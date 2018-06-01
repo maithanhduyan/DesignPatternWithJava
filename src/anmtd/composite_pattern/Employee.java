96bea9c7ef46299d9a9d83ae9b23c5fdd4086c3b
DESKTOP-JF0B3BJ\AnMTD <tiachop0102@gmail.com>
2018-06-01 15:30:10 +0700

    Create Composite Pattern

---
+++ src/anmtd/composite_pattern/Employee.java
@@ -0,0 +1,36 @@
+package anmtd.composite_pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+
+	private String name;
+	private String dept;
+	private int salary;
+	private List<Employee> subordinates;
+
+	public Employee(String name, String dept, int salary) {
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+		subordinates = new ArrayList<Employee>();
+	}
+
+	public void add(Employee e) {
+		subordinates.add(e);
+	}
+
+	public void remove(Employee e) {
+		subordinates.remove(e);
+	}
+
+	public List<Employee> getSubordinates() {
+		return subordinates;
+	}
+
+	public String toString() {
+		return ("Employee :[ Name : " + name + ", dept: " + dept + ", salary: " + salary + " ]");
+	}
+
+}
