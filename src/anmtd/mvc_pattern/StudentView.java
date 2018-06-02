eaf775e7196f02b2d0c3439d25a62f51ec2cc2f1
DESKTOP-JF0B3BJ\AnMTD <tiachop0102@gmail.com>
2018-06-02 10:48:17 +0700

    Create MVC Pattern

---
+++ src/anmtd/mvc_pattern/StudentView.java
@@ -0,0 +1,10 @@
+package anmtd.mvc_pattern;
+
+public class StudentView {
+
+	public void printStudentDetails(String studentName, String studentRollNo) {
+		System.out.println("Student: ");
+		System.out.println("Name: " + studentName);
+		System.out.println("Roll No: " + studentRollNo);
+	}
+}
