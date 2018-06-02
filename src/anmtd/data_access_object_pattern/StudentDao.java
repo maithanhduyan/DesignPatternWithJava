b9015887da8775975c7b4d1c84f962d9bd962459
DESKTOP-JF0B3BJ\AnMTD <tiachop0102@gmail.com>
2018-06-02 12:10:58 +0700

    Create Data Access Object Pattern

---
+++ src/anmtd/data_access_object_pattern/StudentDao.java
@@ -0,0 +1,14 @@
+package anmtd.data_access_object_pattern;
+
+import java.util.List;
+
+public interface StudentDao {
+	
+	public List<Student> getAllStudents();
+
+	public Student getStudent(int rollNo);
+
+	public void updateStudent(Student student);
+
+	public void deleteStudent(Student student);
+}
